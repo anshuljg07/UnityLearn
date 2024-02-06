1f95be3f6bb2520f6dda04790a21497830ea5eb8
anshuljg07 <anshuljg07@gmail.com>
2024-02-06 09:09:59 -0600

    added dog catching ball game

---
+++ Challenge 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float spawnDelay = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnDelay += Time.deltaTime;
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && (spawnDelay > 1))
+        {
+            spawnDelay = 0.0f;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
